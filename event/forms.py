fab6df14dea3208ccae38322d90cff15a30825ba
Pietro Bonechi <p.bonechi1@gmail.com>
2024-06-16 20:09:32 +0200

    Added toast for manage error and info Added list of partecipants for managed own events, and button for remove Need to fix edit button

--- event/forms.py
+++ event/forms.py
@@ -1,11 +1,26 @@
 import datetime
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
+
+from event.models import Event
 
 
 class EventForm(forms.Form):
+
     name = forms.CharField()
+    max_participants = forms.IntegerField()
     description = forms.CharField()
     date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
     location = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_action = 'Submit'
+        self.helper.add_input(Submit('submit', 'Submit', css_class='btn btn-primary'))
+
+    class Meta:
+        model = Event
+        fields = '__all__'
