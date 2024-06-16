f010537b89df911c00a6d57fda3ec6e565ffb5e1
Pietro Bonechi <p.bonechi1@gmail.com>
2024-06-16 14:25:05 +0200

    Fixed model event, default value

--- event/models.py
+++ event/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -6,10 +8,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-    description = models.TextField()
+    location = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateField(default=datetime.date.today)
+    time = models.TimeField(default='00:00:00')
+    description = models.TextField(null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     participants = models.ManyToManyField(User, related_name="participants")
 
